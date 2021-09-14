5b6cd0cef19d58648a41b8890ce4c2127b8e4545
storj-antonio <antonio@storj.io>
2021-09-14 13:22:11 -0400

    This commit implements optional arguements, help, and usage examples. It cleans up error handling, variable declaration, and overall readability. The clean environment flag/feature has been implemented and included.

--- clone-repos.sh
+++ clone-repos.sh
@@ -2,23 +2,60 @@
 set -u
 set -o pipefail
 
+trap Traperr 1 2 3 6
+
+export GOBIN=~/go/bin
+Environment=Test
+Clean=False
 Source="${1:-Github}"
 CurrentRepo="notset"
-Location="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
-echo "Cloning repos in: ${Location}."
-export GOBIN=~/go/bin
+ProjectRoot="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+CloneRepos=("storj" "gateway-mt" "tardigrade-satellite-theme")
 
-traperr() {
+while getopts "he:c" arg; do
+	case $arg in
+		h)
+			Help 
+			;;
+		e)
+			Environment=${OPTARG}
+			echo $Environment
+			;;
+		c)
+			Clean=True
+			;;
+	esac
+done
+
+Help()
+{
+	Usage
+	# echo "Add description of the script functions here."
+	echo
+	echo "Syntax: ${0} [-e|c|h]"
+	echo "options:"
+	echo "e		Force environment to dev"
+	echo "c     Clean environemnt(delete binaries and remove local dependant packages)."
+	echo
+}
+
+Usage()
+{
+	echo "usage: ${0}"
+}
+
+Traperr() {
 	echo "ERROR: ${BASH_SOURCE[1]} near line ${BASH_LINENO[0]} while working with ${CurrentRepo}."
 }
-trap traperr ERR
 
-declare -a CloneRepos=("storj" "gateway-mt" "tardigrade-satellite-theme")
+
+##### Begin Script #####
+echo "Cloning repos in: ${ProjectRoot}."
 
 for val in "${CloneRepos[@]}"; do
 	CurrentRepo=$val
 
-	if [[ -d "${Location}/${CurrentRepo}" ]]; then
+	if [[ -d "${ProjectRoot}/${CurrentRepo}" ]]; then
 		echo "${CurrentRepo} folder already exists, skipping checkout."
 	else
 		if [[ "${Source}" == "Github" ]]; then
@@ -30,14 +67,21 @@ for val in "${CloneRepos[@]}"; do
 		fi
 	fi
 
+	echo "Clean the environment: ${Clean}"
+	if [[ $Clean == "True" ]]; then
+		cd "${ProjectRoot}/${CurrentRepo}"
+		go clean -i all
+	fi
+
 	echo "Installing ${CurrentRepo}..."
-	if [[ -d "${Location}/${CurrentRepo}/cmd/" ]]; then
+	if [[ -d "${ProjectRoot}/${CurrentRepo}/cmd/" ]]; then
 		echo "${CurrentRepo} has cmd directory"
-		cd "${Location}/${CurrentRepo}/"
+		cd "${ProjectRoot}/${CurrentRepo}/"
 		go install -v ./cmd/...
 	else
 		echo "${CurrentRepo} doesn't contain a cmd directory."
 		# Tardigrade Branding
-		# cp -r "${Location}/tardigrade-satellite-theme/europe-west-1/*" "${Location}/storj/web/satellite/"
+		# cp -r "${ProjectRoot}/tardigrade-satellite-theme/europe-west-1/*" "${ProjectRoot}/storj/web/satellite/"
 	fi
+	cd $ProjectRoot
 done
