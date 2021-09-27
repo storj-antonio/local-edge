c62040f9612709b433cdbec7c6bb9c69f875339e
Antonio Franco <antonio@storj.io>
2021-09-27 12:01:12 -0400

    added grab.sh so that binaries need not be compiled

---
+++ grab.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FAIL=0
+
+go install -race -v storj.io/storj/cmd/satellite@latest &
+go install -race -v storj.io/storj/cmd/storagenode@latest &
+go install -race -v storj.io/storj/cmd/storj-sim@latest &
+go install -race -v storj.io/storj/cmd/versioncontrol@latest &
+go install -race -v storj.io/storj/cmd/uplink@latest &
+go install -race -v storj.io/storj/cmd/identity@latest &
+go install -race -v storj.io/storj/cmd/certificates@latest &
+go install -race -v storj.io/storj/cmd/multinode@latest &
+go install -race -v storj.io/gateway@latest &
+
+for job in `jobs -p`
+do
+echo $job
+    wait $job || let "FAIL+=1"
+done
+
+echo $FAIL
+
+if [ "$FAIL" == "0"  ]; then
+	./start-services.sh
+fi
